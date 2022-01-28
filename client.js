49c9267595654dc80445d804b7e9843aea183c7d
Brandon Lanier <brandonjlanier@gmail.com>
2022-01-27 22:34:40 -0600

    Adding jquery functionality

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 $(document).ready(readyNow)
 
+let employees = [];
+
 function readyNow() {
-   $('#submitBtn').on('click', addEmployee)
-    
+   $('#submitBtn').on('click', addEmployee);
+   $('table').on('click', '.deleteBtn', deleteEmp)
+    calculateSal();
 }
 
 function addEmployee() {
+    console.log('in addEmp');
     let el = $('.inputs');
     let employee = {
         firstName: $('#firstNameIn').val(),
@@ -16,4 +20,24 @@ function addEmployee() {
     }
     el.val('');
     employees.push(employee)
+    displayEmp(employee);
+    return employee;
 }
+
+function displayEmp(employee) {
+    let el = $('#tableBody')
+    let first = employee.firstName;
+    let last = employee.lastName;
+    let id = employee.idNumber;
+    let title = employee.title;
+    let salary = employee.salary;
+    el.append(`<tr><td>${first}</td><td>${last}</td><td>${id}</td><td>${title}</td><td>${salary}</td><td><button class="deleteBtn">Remove</button></tr>`)
+}
+
+function deleteEmp(){
+    $(this).closest('tr').remove();
+//   
+}
+
+
+
